779d2c8754fd3340304d4edf360ae758f94bdab1
German Sandoval <gaso.ufo@gmail.com>
2021-01-24 23:05:11 -0600

    Final de la etapa6

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -26,6 +26,27 @@ namespace CoreEscuela
             CargarEvaluaciones();
         }
 
+        public List<ObjetoEscuelaBase> GetObjetoEscuela()
+        {
+            var listaObj = new List<ObjetoEscuelaBase>();
+            listaObj.Add(Escuela);
+            listaObj.AddRange(Escuela.Cursos);
+            
+            foreach (var curso in Escuela.Cursos)
+            {
+                listaObj.AddRange(curso.Asignaturas);
+                listaObj.AddRange(curso.Alumnos);
+
+                foreach (var alumno in curso.Alumnos)
+                {
+                    listaObj.AddRange(alumno.Evaluaciones);
+                }
+            }
+
+            return listaObj;
+        }
+
+        #region Metodos de carga
         private void CargarEvaluaciones()
         {
             Random rnd = new Random();
@@ -102,26 +123,7 @@ namespace CoreEscuela
                 curso.Alumnos = GenerarAlumnosAlAzar(cantidadRandom);
             }
         }
-    
-        public List<ObjetoEscuelaBase> GetObjetoEscuela()
-        {
-            var listaObj = new List<ObjetoEscuelaBase>();
-            listaObj.Add(Escuela);
-            listaObj.AddRange(Escuela.Cursos);
-            
-            foreach (var curso in Escuela.Cursos)
-            {
-                listaObj.AddRange(curso.Asignaturas);
-                listaObj.AddRange(curso.Alumnos);
 
-                foreach (var alumno in curso.Alumnos)
-                {
-                    listaObj.AddRange(alumno.Evaluaciones);
-                }
-            }
-
-            return listaObj;
-        }
-
+        #endregion
     }
 }
