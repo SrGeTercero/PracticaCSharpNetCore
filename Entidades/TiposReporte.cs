b07006ae6b7ae535e7e4f29f80a73ee987d4f356
German Sandoval <gaso.ufo@gmail.com>
2021-04-05 21:21:20 -0600

    metod para mostrar los reportes en consola

---
+++ Entidades/TiposReporte.cs
@@ -0,0 +1,15 @@
+namespace CoreEscuela.Entidades
+{
+    public enum TiposReporte
+    {
+        GetListaEvaluaciones,
+
+        GetListAsignaturas,
+
+        GetDiccionarioEvaluacionesXAsignatura,
+
+        GetPromedioAlumnoXAsignatura,
+
+        GetPromedioAlumnoXAsignatura_Cantidad,
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,52 +9,55 @@ using static System.Console;
 namespace CoreEscuela
 {
     class Program
-    {
+    {   
+        public static EscuelaEngine engine = new EscuelaEngine();
         static void Main(string[] args)
         {
-            AppDomain.CurrentDomain.ProcessExit += AccionDelEvento;
-            AppDomain.CurrentDomain.ProcessExit += (o, s) => Printer.Pitar(2000, 1000, 1);
+            //AppDomain.CurrentDomain.ProcessExit += AccionDelEvento;
+            //AppDomain.CurrentDomain.ProcessExit += (o, s) => Printer.Pitar(2000, 1000, 1);
             //AppDomain.CurrentDomain.ProcessExit -= AccionDelEvento;
             
-            var engine = new EscuelaEngine();
-            engine.Inicializar();
             Printer.WriteTitle("Bienvenidos a la escuela");
-            
-            var reporteador = new Reporteador(engine.GetDiccionarioDeObjetos());
+            engine.Inicializar();
 
-            var evalList = reporteador.GetListaEvaluaciones();
+            ImprimirMenuDeReportes(out int reporteElegido, out int cantidadOpcionMenu);
+            MostrarReporte(reporteElegido, cantidadOpcionMenu);
 
-            var asigList = reporteador.GetListAsignaturas();
+            // var evalList = reporteador.GetListaEvaluaciones();
 
-            var listaEvalXAsig = reporteador.GetDiccionarioEvaluacionesXAsignatura();
+            // var asigList = reporteador.GetListAsignaturas();
 
-            var listaPromXAsig = reporteador.GetPromedioAlumnoXAsignatura();
+            // var listaEvalXAsig = reporteador.GetDiccionarioEvaluacionesXAsignatura();
 
-            var listaPromXAsig_Cantidad = reporteador.GetPromedioAlumnoXAsignatura(5);
+            // var listaPromXAsig = reporteador.GetPromedioAlumnoXAsignatura();
 
-            Printer.WriteTitle("Captura de una evaluación por consola");
-            var newEval = new Evaluación();
-            string nombre, notaString;
-            float nota;
+            // var listaPromXAsig_Cantidad = reporteador.GetPromedioAlumnoXAsignatura(5);
 
-            WriteLine("Ingrese el nombre de la evaluación");
-            Printer.PresioneENTER();
-            nombre = Console.ReadLine();
+            #region codigo Ejemplo para trabajar con try, catch, finally
 
-            if(string.IsNullOrWhiteSpace(nombre))
-            {
-                Printer.WriteTitle("El valor del nombre no puede ser vacio");
-                WriteLine("Saliendo del programa");
-            }
-            else
-            {
-                newEval.Nombre = nombre.ToLower();
-                WriteLine("El nombre de la evaluación ha sido ingresado correctamente.");
-            }
+            // Printer.WriteTitle("Captura de una evaluación por consola");
+            // var newEval = new Evaluación();
+            // string nombre, notaString;
+            // float nota;
 
-            WriteLine("Ingrese la nota de la evaluación");
-            Printer.PresioneENTER();
-            notaString = Console.ReadLine();
+            // WriteLine("Ingrese el nombre de la evaluación");
+            // Printer.PresioneENTER();
+            // nombre = Console.ReadLine();
+
+            // if(string.IsNullOrWhiteSpace(nombre))
+            // {
+            //     Printer.WriteTitle("El valor del nombre no puede ser vacio");
+            //     WriteLine("Saliendo del programa");
+            // }
+            // else
+            // {
+            //     newEval.Nombre = nombre.ToLower();
+            //     WriteLine("El nombre de la evaluación ha sido ingresado correctamente.");
+            // }
+
+            // WriteLine("Ingrese la nota de la evaluación");
+            // Printer.PresioneENTER();
+            // notaString = Console.ReadLine();
 
             // if(string.IsNullOrWhiteSpace(notaString))
             // {
@@ -63,34 +66,154 @@ namespace CoreEscuela
             // }
             // else
             // {
-                try
-                {
-                    newEval.Nota = float.Parse(notaString);
-                    if (newEval.Nota < 0 || newEval.Nota > 5)
-                    {
-                        throw new ArgumentOutOfRangeException("La nota debe estar entre 0 y 5");
-                    }
-                    WriteLine("La nota de la evaluación ha sido ingresado correctamente.");
-                    return;
-                }
-                catch(ArgumentOutOfRangeException argE)
-                {
-                    Printer.WriteTitle(argE.Message);
-                    WriteLine("Saliendo del programa");
-                }
+                // try
+                // {
+                //     newEval.Nota = float.Parse(notaString);
+                //     if (newEval.Nota < 0 || newEval.Nota > 5)
+                //     {
+                //         throw new ArgumentOutOfRangeException("La nota debe estar entre 0 y 5");
+                //     }
+                //     WriteLine("La nota de la evaluación ha sido ingresado correctamente.");
+                //     return;
+                // }
+                // catch(ArgumentOutOfRangeException argE)
+                // {
+                //     Printer.WriteTitle(argE.Message);
+                //     WriteLine("Saliendo del programa");
+                // }
                 // catch (Exception)
                 // {
                 //     Printer.WriteTitle("El valor de nota no es un número valido");
                 //     WriteLine("Saliendo del programa");
                 // }
-                finally
-                {
-                    Printer.WriteTitle("FINALLY");
-                    Printer.Pitar(2500, 500, 3);
-                }
+                // finally
+                // {
+                //     Printer.WriteTitle("FINALLY");
+                //     Printer.Pitar(2500, 500, 3);
+                // }
             //}
+
+#endregion
+        
+        
+        }
+        
+        private static void ImprimirMenuDeReportes(out int reporteElegido, out int cantidadOpcionMenu){
+            Printer.WriteTitle("Menu de reportes");
+
+            var cantidadReportes = Enum.GetNames(typeof(TiposReporte)).Count();
+            reporteElegido = 0;
+            cantidadOpcionMenu = 1;
+
+            foreach (var reporte in Enum.GetValues(typeof(TiposReporte)))
+            {
+                WriteLine($"Presione {cantidadOpcionMenu} para ver el reporte: {reporte}");
+                if(cantidadOpcionMenu < cantidadReportes) cantidadOpcionMenu++;
+            }
+
+            try
+            {
+                int teclaPresionada = Console.ReadKey().KeyChar;
+                reporteElegido = Convert.ToInt32((System.Convert.ToChar(teclaPresionada)).ToString());
+            }catch(Exception e){
+                WriteLine();
+                WriteLine($"Unicamente se permite elegir opciones con valor numerico {e.Message}");
+            }
         }
 
+        public static void MostrarReporte(int reporteElegido, int cantidadOpciones){
+            //WriteLine(reporteElegido);
+            //WriteLine(cantidadOpciones);
+            try
+            {
+                if(reporteElegido > cantidadOpciones || reporteElegido < (cantidadOpciones - (cantidadOpciones-1)))
+                {
+                    throw new RankException();
+                }
+                else
+                {
+                    var tipoReporte = Enum.GetName(typeof(TiposReporte),reporteElegido-1);
+                    //WriteLine(tipoReporte);
+                    var reporteador = new Reporteador(engine.GetDiccionarioDeObjetos());
+                    switch (tipoReporte)
+                    {
+                        case "GetListaEvaluaciones":
+                            WriteLine();
+                            var evalList = reporteador.GetListaEvaluaciones();
+                            foreach (var eval in evalList)
+                            {
+                                WriteLine($"Nombre: {eval.Nombre}, Id: {eval.UniqueId}, Alumno: {eval.Alumno.Nombre}, Asignatura: {eval.Asignatura.Nombre}, Nota: {eval.Nota}");
+                            }
+                        break;
+                        case "GetListAsignaturas":
+                            WriteLine();
+                            var asigList = reporteador.GetListAsignaturas();
+                            foreach (var asig in asigList)
+                            {
+                                WriteLine($"Asignatura: {asig}");
+                            }
+                        break;
+                        case "GetDiccionarioEvaluacionesXAsignatura":
+                            WriteLine();
+                            var listaEvalXAsig = reporteador.GetDiccionarioEvaluacionesXAsignatura();
+                            foreach (var EvalXAsig in listaEvalXAsig)
+                            {
+                                Printer.WriteTitle($"{EvalXAsig.Key}");
+                                foreach (var eval in EvalXAsig.Value)
+                                {
+                                    WriteLine($"Nombre: {eval.Nombre}, Id: {eval.UniqueId}, Alumno: {eval.Alumno.Nombre}, Asignatura: {eval.Asignatura.Nombre}, Nota: {eval.Nota}");
+                                }
+                            }
+                        break;
+                        case "GetPromedioAlumnoXAsignatura":
+                            WriteLine();
+                            var listaPromXAsig = reporteador.GetPromedioAlumnoXAsignatura();
+                            foreach (var PromXAsig in listaPromXAsig)
+                            {
+                                Printer.WriteTitle($"{PromXAsig.Key}");
+                                foreach (var eval in PromXAsig.Value)
+                                {
+                                    WriteLine($"IdAlumno: {eval.alumnoId}, NombreAlumno: {eval.alumnoNombre}, Promedio: {eval.promedio}");
+                                }
+                            }
+                        break;
+                        case "GetPromedioAlumnoXAsignatura_Cantidad":
+                            WriteLine();
+                            WriteLine("Ingrese la cantidad de alumnos que desea ver:");
+                            try
+                            {
+                                int teclaPresionada = Console.ReadKey().KeyChar;
+                                int cantidadAlumnos = Convert.ToInt32((System.Convert.ToChar(teclaPresionada)).ToString());
+                                WriteLine();
+                                var listaPromXAsig_Cantidad = reporteador.GetPromedioAlumnoXAsignatura(cantidadAlumnos);
+                                foreach (var PromXAsig in listaPromXAsig_Cantidad)
+                                {
+                                    Printer.WriteTitle($"{PromXAsig.Key}");
+                                    foreach (var eval in PromXAsig.Value)
+                                    {
+                                        WriteLine($"IdAlumno: {eval.alumnoId}, NombreAlumno: {eval.alumnoNombre}, Promedio: {eval.promedio}");
+                                    }
+                                }
+                            }catch(Exception e){
+                                WriteLine();
+                                WriteLine($"Unicamente se permite elegir opciones con valor numerico {e.Message}");
+                            }
+                        break;
+                        default:
+                        break;
+                    }                    
+                }
+            }
+            catch(RankException ex)
+            {
+                WriteLine($"ERROR: Ha elegido una opción que no existe! -> {ex.Message}");
+            }
+            catch(Exception ex){
+                WriteLine($"ERROR:  -> {ex.Message}");
+            }
+            
+        }
+        
         private static void AccionDelEvento(object sender, EventArgs e)
         {
             Printer.WriteTitle("SALIENDO");
